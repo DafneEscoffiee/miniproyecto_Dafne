f95b98c2510b5f6c064667b3cae2f40071bbc9dd
DafneEscoffiee <84392932+DafneEscoffiee@users.noreply.github.com>
2021-05-27 10:09:58 -0500

    Update App.php

--- codeingniter/app/Config/App.php
+++ codeingniter/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://http://cerounodigital.com/btc06/dafne/codeingniter/public/';
+	public $baseURL = 'http://cerounodigital.com/btc06/dafne/codeingniter/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
