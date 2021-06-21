e95c657e5f6fb5730ebee45c3363d76cd856b816
DafneEscoffiee <84392932+DafneEscoffiee@users.noreply.github.com>
2021-06-21 08:22:36 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -3,32 +3,78 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class="jumbotron">
-  <h1 class="display-4">Dafne Escoffie!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <h1>Agregar Registro</h1>
+     <div class="row">
+       <div class="col-sm-12">
+         <form action="<?PHP echo base_url('index.php/crear')?>"method="POST">
+           <label for="nombre">Nombre:</label>
+           <input type="text" id="nombre" name="nombre" class="form-control" required>
+           <label for="apaterno">Apellido Paterno:</label>
+           <input type="text" id="apaterno" name="apaterno" class="form-control" required>
+           <label for="amaterno">Apellido Materno:</label>
+           <input type="text" id="amaterno" name="amaterno" class="form-control" required>
+           <button class="btn btn-primary form-control">Guardar</button>
+         </form>
+       </div>
+      </div>
+       <div class="row">
+         <div class="col-sm-12">
+            <div class="table table=resonsive">
+              <table class="table table-hover table-bordered">
+                <tr>
+                  <th>Nombre</th>
+                  <th>Apellido paterno</th>
+                  <th>Apellido materno</th>
+                  <th>Editar</th>
+                  <th>Eliminar</th>
+                </tr>
+                <?php foreach($datos as $dato): ?>
+                <tr> 
+                  <th><?php echo $dato->nombre ?></th>
+                  <th><?php echo $dato->apellido_paterno ?></th>
+                  <th><?php echo $dato->apellido_materno ?></th>
+                  <th> <a href="<?php echo base_url('/index.php/obtenerInformacion') .'/'.$dato->id ?> "class="btn btn-sm btn-warning">Editar</a> </th>
+                  <th> <a href="<?php echo base_url('/index.php/eliminar').'/'.$dato->id ?>" class="btn btn-sm btn-danger">Eliminar</a> </th>
+                </tr>
+                <?php endforeach ?>
+              </table>
+           </div>
+         </div>
+      </div>
+                  
+    </div>
+   
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+      let mensaje == '<?php echo $mensaje; ?>';
+      if (mensaje == '0'){
+        swal (':D','registro insertado correctamente', 'success');
+      }else if(mensaje =='1'){
+        swal(':(', 'no fue posible insertar registro correctamente', 'error');
+      }else if(mensaje =='2'){
+        swal(':D', 'registro actualizado correctamente', 'success');
+      }else if(mensaje =='3'){
+        swal(':(', 'no fue posible actualizar el registro correctamente', 'error');
+      }else if(mensaje =='4'){
+        swal(':D', 'registro eliminado correctamente', 'success');
+      }else if(mensaje =='5'){
+        swal(':(', 'mp fue posible eliminar el registro correctamente', 'error');
+      }
+    </script>
+        
   </body>
 </html>
